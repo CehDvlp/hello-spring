cc875dfbbd2ccdc5be10da4f851b66795329bd95
CehDvlp <nyih1719@gmail.com>
2021-03-15 18:54:26 +0900

    Form과 PostMapping을 통한 회원 등록 기능 테스트

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,7 +1,6 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
@@ -10,6 +9,7 @@ public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
+    @Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
